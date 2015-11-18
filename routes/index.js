e4118bbbc2d8b7db1cbcd4bc949e6536c93baa87
powolnymarcel <powolnymarcel@gmail.com>
2015-11-18 02:08:01 +0100

    templates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Veuillez vous connecter' });
 });
+
+
+/* GET home page. */
+router.get('/inscription', function(req, res, next) {
+	res.render('inscription', { title: 'inscription' });
+});
+
+/* GET home page. */
+router.get('/dashboard', function(req, res, next) {
+	res.render('dashboard', { title: 'dashboard',layout:'dashboard_layout' });
+});
+
+
 
 module.exports = router;
